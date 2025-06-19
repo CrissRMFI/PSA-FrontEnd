a15cb220e60b26fe12afd81a42a455e92e57c77e
ffraga98 <ferfraga2@gmail.com>
2025-06-19 00:04:22 -0300

    log

--- src/api/serviciosExternos.js
+++ src/api/serviciosExternos.js
@@ -2,12 +2,14 @@ const URL_CLIENTES = `${process.env.SUPPORT_MODULE_BACKEND_URL}/external/clients
 const URL_RESPONSABLES = `${process.env.SUPPORT_MODULE_BACKEND_URL}/external/resources`;
 
 export const getClientes = async () => {
+  console.log(URL_CLIENTES)
   const res = await fetch(URL_CLIENTES);
   if (!res.ok) throw new Error("Error al obtener clientes");
   return res.json();
 };
 
 export const getResponsables = async () => {
+  console.log(URL_RESPONSABLES)
   const res = await fetch(URL_RESPONSABLES);
   if (!res.ok) throw new Error("Error al obtener responsables");
   return res.json();
