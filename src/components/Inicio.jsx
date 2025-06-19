164e3dfadb9b94b3455a5be36aa7e43b5e32b529
ffraga98 <ferfraga2@gmail.com>
2025-06-19 19:58:41 -0300

    chg: nombre app

--- src/components/Inicio.jsx
+++ src/components/Inicio.jsx
@@ -5,7 +5,8 @@ import AreaProyecto from "./Botones/AreaProyecto";
 export const Inicio = () => {
   return (
     <div className="w-full h-screen flex flex-col justify-center items-center">
-      <h1 className="text-[60px] font-extrabold mb-10">BIENVENIDO A PSA</h1>
+      <h1 className="text-[40px] font-extrabold mb-10 ">Bienvenido a</h1>
+      <h1 className="text-[60px] font-extrabold mb-10"> Pro Sync Analytics</h1>
       <div className="flex justify-evenly w-1/2">
         <AreaSoporte />
         <AreaProyecto />
