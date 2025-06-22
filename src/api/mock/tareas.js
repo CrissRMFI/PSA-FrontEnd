8e417e13c9a6b0bc461cd7dd63fa4bd6c5a31dd6
fabivrp <fromerop@fi.uba.ar>
2025-06-22 19:40:06 -0300

    Agrego mock para tareas

---
+++ src/api/mock/tareas.js
@@ -0,0 +1,37 @@
+const tareas = [
+    {
+        idticket: 1,
+        id: 1,
+        titulo: "Revisar SLA",
+        descripcion: "Verificar si el SLA está correctamente cargado.",
+        estado: "En progreso",
+        prioridad: "Alta",
+        responsable: "Lucía Ramírez",
+        idProyecto: 3,
+        nombreProyecto: "Mesa de Soporte Nivel 1"
+      },
+      {
+        idticket: 1,
+        id: 2,
+        titulo: "Actualizar base de conocimiento",
+        descripcion: "Agregar nueva solución para errores de conexión.",
+        estado: "Pendiente",
+        prioridad: "Media",
+        responsable: "Carlos Torres",
+        idProyecto: 3,
+        nombreProyecto: "Mesa de Soporte Nivel 1"
+      },
+      {
+        idticket: 1,
+        id: 3,
+        titulo: "Test 3",
+        descripcion: "Agregar nueva solución para errores de conexión.",
+        estado: "Pendiente",
+        prioridad: "Media",
+        responsable: "Carlos Torres",
+        idProyecto: 3,
+        nombreProyecto: "Mesa de Soporte Nivel 1"
+      }
+  ];
+  
+export default tareas;
